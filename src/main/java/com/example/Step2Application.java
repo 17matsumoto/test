681bc355c616ffed05305b30be93b0026ce92c74
17matsumoto <17yuya.baseball@gmail.com>
2022-11-03 17:04:52 +0900

    first commit

---
+++ src/main/java/com/example/Step2Application.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Step2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Step2Application.class, args);
+	}
+
+}
